c03a57771468c36572f3490a8a1cb14f28779618
tomgodsalo <tomgodsalo1@gmail.com>
2025-02-07 14:47:15 +0530

    Initial commit

---
+++ Assets/Push.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    public GameObject pushReady = null;
+    public List<Transform> items = new List<Transform>();
+    private int layerMask = 0;
+
+    public void Start()
+    {
+        layerMask = ~LayerMask.GetMask("ground", "player");
+    }
+
+    private void FixedUpdate()
+    {
+        if (pushReady != null)
+        {
+            AllMightyPush(pushReady.transform.position, 10);
+        }
+        if (Input.GetKey(KeyCode.R))
+            StartCoroutine(SmoothRase());
+        if (Input.GetKey(KeyCode.T))
+            ApplyForwardImpulse(5);
+
+    }
+
+    public void AllMightyPush(Vector3 center, float radius)
+    {
+        int maxColliders = 10;
+        Collider[] hits = new Collider[maxColliders];
+        int numColliders = Physics.OverlapSphereNonAlloc(center, radius, hits, layerMask);
+
+        for (int i = 0; i < numColliders; i++)
+        {
+            Transform hitTransform = hits[i].GetComponent<Transform>();
+            if (hitTransform != null && !items.Contains(hitTransform))
+            {
+                items.Add(hitTransform);
+            }
+        }
+    }
+
+    private IEnumerator SmoothRase()
+    {
+        float duration = 2f; // Time taken to move objects
+        float elapsedTime = 0f;
+
+        Dictionary<Transform, Vector3> startPositions = new Dictionary<Transform, Vector3>();
+        Dictionary<Transform, Vector3> targetPositions = new Dictionary<Transform, Vector3>();
+
+        foreach (Transform item in items)
+        {
+            if (item != null)
+            {
+                startPositions[item] = item.position;
+                targetPositions[item] = new Vector3(
+                    pushReady.transform.position.x,
+                    pushReady.transform.position.y ,
+                    pushReady.transform.position.z
+                );
+            }
+        }
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration;
+
+            foreach (Transform item in items)
+            {
+                if (item != null)
+                {
+                    item.position = Vector3.Lerp(startPositions[item], targetPositions[item], t);
+                }
+            }
+
+            yield return null;
+        }
+
+        foreach (Transform item in items)
+        {
+            if (item != null)
+            {
+                item.position = targetPositions[item]; // Ensure final position is exact
+            }
+        }
+    }
+    public void ApplyForwardImpulse(float forceAmount)
+    {
+        foreach (Transform item in items)
+        {
+            if (item != null)
+            {
+
+                Rigidbody rb = item.GetComponent<Rigidbody>(); // Get Rigidbody component
+
+                if (rb != null)
+                {
+                    Vector3 pushDirection = Vector3.forward;   /*Vector3 pushDirection = (item.position - pushReady.transform.position).normalized;*/ // Direction away from pushReady
+                    rb.AddForce(pushDirection * forceAmount, ForceMode.Impulse); // Apply impulse force
+                }
+            }
+        }
+    }
+
+}
