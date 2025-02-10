36ce5966fef37099705940fec68ff26a22885522
Tom Godsalo <100946325+Tee8590@users.noreply.github.com>
2025-02-10 19:37:52 +0530

    new Update0.1

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,17 +1,67 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    public GameObject itemPrefab = null;
+    public GameObject Player = null;
     public static Item Instance;
-    public int amountOfObj = 20;
+    public List<GameObject> itemsToPush = new List<GameObject>();
+    public int amountOfObj = 80;
+    private Vector3 position;
+    private Quaternion rotation;
+    private void Awake()
+    {
+        if (Instance == null)
+           
+            Instance = this;
+       /* Debug.Log(Instance);*/
+    }
     void Start()
     {
+        if (itemPrefab == null)
+            Debug.Log("item null");
+       
+        for (int i = 0; i < amountOfObj; i++)
+        {
+            GameObject item = Instantiate(itemPrefab, position,rotation);
+           /* Debug.Log("item");*/
+            item.gameObject.SetActive(false);
+            itemsToPush.Add(item);
+            /*Debug.Log("itemsToPush");*/
+        }
+    }
+    public void FixedUpdate()
+    {
+        position = SpawnPosition();
+        rotation = RandomRotation();
+    }
+    public GameObject GetItemsToPush()
+    {
+        for(int i = 0;i < itemsToPush.Count;i++)
+        {
+            if (!itemsToPush[i].gameObject.activeInHierarchy)
+            {
+                return itemsToPush[i];
+            }
+        }
+        return null;
+    }
+    public Vector3 SpawnPosition()
+    {
+       
+        position.y = 1f;
+        position.x = Player.transform.position.x + Random.Range(Player.transform.position.x , Player.transform.position.x-5f);
+        position.z = Player.transform.position.z + Random.Range(Player.transform.position.z , Player.transform.position.z+5f);
+        return position;
+    }
+    public Quaternion RandomRotation()
+    {
+        float x = Random.Range(0f, 360f);
+        float y = Random.Range(0f, 360f);
+        float z = Random.Range(0f, 360f);
+        return Quaternion.Euler(x, y, z);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Push.cs
+++ Assets/Scripts/Push.cs
@@ -6,6 +6,7 @@ public class Push : MonoBehaviour
 {
     public GameObject pushReady = null;
     public List<Transform> items = new List<Transform>();
+    private GameObject itemObj;
     private int layerMask = 0;
 
     Dictionary<Transform, Vector3> startPositions;
@@ -13,16 +14,22 @@ public class Push : MonoBehaviour
     public void Start()
     {
         layerMask = ~LayerMask.GetMask("ground", "player");
+     /*   SpawnItems();*/
     }
 
     private void FixedUpdate()
     {
+        
         if (pushReady != null)
         {
             AllMightyPush(pushReady.transform.position, 10);
+            
         }
         if (Input.GetKey(KeyCode.R))
+        {   
+
             StartCoroutine(SmoothRase());
+        }
         if (Input.GetKey(KeyCode.T))
             ApplyForwardImpulse(5);
 
@@ -40,6 +47,7 @@ public class Push : MonoBehaviour
             if (hitTransform != null && !items.Contains(hitTransform))
             {
                 items.Add(hitTransform);
+                /*Debug.Log("hit");*/
             }
         }
     }
@@ -52,7 +60,7 @@ public class Push : MonoBehaviour
 
           startPositions = new Dictionary<Transform, Vector3>();
          targetPositions = new Dictionary<Transform, Vector3>();
-
+        SpawnItems();
         foreach (Transform item in items)
         {
             if (item != null)
@@ -110,11 +118,25 @@ public class Push : MonoBehaviour
                 {
                     Vector3 pushDirection = Vector3.forward;   /*Vector3 pushDirection = (item.position - pushReady.transform.position).normalized;*/ // Direction away from pushReady
                     rb.AddForce(pushDirection * forceAmount, ForceMode.Impulse); // Apply impulse force
-                    toRemove.Add(item);
+                    itemObj.gameObject.SetActive(false);
+                    /*toRemove.Add(item);*/
                 }
             }
         }
        
     }
-
+    private void SpawnItems()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            GameObject itemObj = Item.Instance.GetItemsToPush();
+            if (itemObj != null)
+            {
+                itemObj.SetActive(false); // Ensure it's inactive before setting properties
+                itemObj.transform.position = Item.Instance.SpawnPosition();
+                itemObj.transform.rotation = Item.Instance.RandomRotation();
+                itemObj.SetActive(true);
+            }
+        }
+    }
 }
