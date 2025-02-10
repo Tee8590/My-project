753f29dcc7fbf19c5743558f28976c3fbd813c9b
Tom Godsalo <100946325+Tee8590@users.noreply.github.com>
2025-02-10 13:22:29 +0530

    fixed all bugs except lost key

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static Item Instance;
+    public int amountOfObj = 20;
     void Start()
     {
         
